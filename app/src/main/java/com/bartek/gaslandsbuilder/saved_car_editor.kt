20ac7d537f409e59f0f6b455aca35e71d3f1d224
BartlomiejF <bartlomiej.fliszkiewicz@gmail.com>
2021-01-07 22:21:29 +0100

    branch created

---
+++ app/src/main/java/com/bartek/gaslandsbuilder/saved_car_editor.kt
@@ -0,0 +1,11 @@
+package com.bartek.gaslandsbuilder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class saved_car_editor : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_saved_car_editor)
+    }
+}
